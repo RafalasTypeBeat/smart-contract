64763cbde49cb08663ebd98c24c204eab070f38d
Rapolas Kairys <rapolas.kairys@gmail.com>
2023-01-05 02:09:58 +0200

    finished smart contract

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const Marketplace = artifacts.require('./Marketplace.sol')
 
 require('chai')
@@ -42,9 +44,51 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
       assert.equal(event.price, '1000000000000000000', 'price is correct')
       assert.equal(event.owner, seller, 'owner is correct')
       assert.equal(event.purchased, false, 'purchase is correct')
-
       await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller}).should.be.rejected;
       await marketplace.createProduct('iPhone 15', 0, { from: seller}).should.be.rejected;
     })
+
+    it('lists products', async () => {
+      const product = await marketplace.products(productCount)
+      assert.equal(product.id.toNumber(), productCount.toNumber(), 'id is correct')
+      assert.equal(product.name, 'iPhone 15', 'name is correct')
+      assert.equal(product.price, '1000000000000000000', 'price is correct')
+      assert.equal(product.owner, seller, 'owner is correct')
+      assert.equal(product.purchased, false, 'purchase is correct')
+    })
+
+    it('sells products', async () => {
+
+      let oldSellerBalance
+      oldSellerBalance = await web3.eth.getBalance(seller)
+      oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+      oldSellerBalance
+      result = await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether')})
+
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+      assert.equal(event.name, 'iPhone 15', 'name is correct')
+      assert.equal(event.price, '1000000000000000000', 'price is correct')
+      assert.equal(event.owner, buyer, 'owner is correct')
+      assert.equal(event.purchased, true, 'purchase is correct')
+
+      let newSellerBalance
+      newSellerBalance = await web3.eth.getBalance(seller)
+      newSellerBalance = new web3.utils.BN(newSellerBalance)
+      
+      let price
+      price = web3.utils.toWei('1', 'Ether')
+      price = new web3.utils.BN(price)
+
+      const expectedBalance = oldSellerBalance.add(price)
+
+      assert.equal(newSellerBalance.toString(), expectedBalance.toString())
+
+      await marketplace.purchaseProduct(99, { from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+      await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('0.5', 'Ether')}).should.be.rejected;
+      await marketplace.purchaseProduct(productCount, { from: deployer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+      await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+    })
+
   })
 })
